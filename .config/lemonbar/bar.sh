11f3f39ed16638ac58126cbe428f844439d5ccf2
procub3r <procub3r@protonmail.com>
2022-09-02 20:12:00 +0530

    Update lemonbar config

--- .config/lemonbar/bar.sh
+++ .config/lemonbar/bar.sh
@@ -19,19 +19,19 @@ done < <(bspc subscribe report) &
 # window title
 while read -r line; do
     echo $line > $fifo
-done < <(xtitle -s -f "T%s\n" -t 52) &
+done < <(xtitle -s -f "T%s\n" -t 80) &
 
 # date
 while :; do
-    echo "D$(date '+%I:%M:%S %p')" > $fifo
+    echo "D$(date '+%I:%M %p | %a, %d %b')" > $fifo
     sleep 1
 done &
 
-# battery
-while :; do
-    echo "B$(acpi -b)" > $fifo
-    sleep 20
-done &
+# # battery
+# while :; do
+#     echo "B$(acpi -b)" > $fifo
+#     sleep 20
+# done &
 
 wm_clickable_desktop() {
     echo "%{A:bspc desktop -f $1:} $2 %{A}"
@@ -41,38 +41,56 @@ wm_scrollable_desktops() {
     echo "%{A4:bspc desktop -f prev.local:}%{A5:bspc desktop -f next.local:}$1%{A}%{A}"
 }
 
+wm_togglable_layout() {
+    echo "%{A:bspc desktop -l next:}$1%{A}"
+}
+
 barbg="#21232b"
 barfg="#e1e3e4"
-
-wmfocusb="#9ed06c"
-wmfocusf=$barbg
-wmurgentb="#fb617e"
-wmurgentf=$barbg
+grey="#666666"
+red="#fb617e"
 
 # read fifo > process and format > render with lemonbar
 tail --follow=name $fifo | while read -r line; do
     case $line in
         D*) date=${line:1} ;;
         W*)
-            state=${line//[^oOfF]/}
-            state=${state::$dcount}
             wm=
-            for (( i=0; i<${#state}; i++ )); do
-                indx=$(($i+1))
-                case ${state:$i:1} in
-                    f)   wm="$wm$(wm_clickable_desktop $indx "_")" ;;
-                    o)   wm="$wm$(wm_clickable_desktop $indx $indx)" ;;
-                    u)   wm="$wm$(wm_clickable_desktop $indx "$indx!")" ;;
-                    O|F) wm="$wm%{B$wmfocusb}%{F$wmfocusf}$(wm_clickable_desktop $indx $indx)%{F-}%{B-}" ;;
-                    U)   wm="$wm%{B$wmurgentb}%{F$wmurgentf}$(wm_clickable_desktop $indx $indx)%{F-}%{B-}" ;;
+            IFS=':'
+            items=($line)
+            for i in "${items[@]}"; do
+                val=${i:1}
+                case $i in
+                    o*) wm="$wm%{F$grey}$(wm_clickable_desktop $val $val)%{F-}" ;;
+                    f*) wm="$wm%{F$grey}$(wm_clickable_desktop $val "_")%{F-}" ;;
+                    u*) wm="$wm%{F$red}$(wm_clickable_desktop $val "$val!")%{F-}" ;;
+                    O*) wm="$wm$(wm_clickable_desktop $val $val)" ;;
+                    F*) wm="$wm$(wm_clickable_desktop $val "_")" ;;
+                    U*) wm="$wm$(wm_clickable_desktop $val "$val!")" ;;
+                    L*)
+                        case $val in
+                            T) wm="$wm $(wm_togglable_layout "[]=")" ;; # tiled
+                            M) wm="$wm $(wm_togglable_layout "[M]")" ;; # monocle
+                        esac ;;
+                    T*)
+                        case $val in
+                            T) ;; # tiled
+                            F) wm="$wm ><>" ;; # floating
+                            P) wm="$wm ][=" ;; # pseudo-tiled
+                            *) wm="$wm $val" ;;
+                        esac ;;
                     *) ;;
                 esac
             done
             wm=$(wm_scrollable_desktops "$wm") ;;
-        T*) title=${line:1} ;;
-        B*) battery=${line:1} ;;
+        T*)
+            title=${line:1}
+            case $title in
+                "") title="[PROCUB3R'S SYSTEM]" ;;
+            esac ;;
+        # B*) battery=${line:1} ;;
         CLOSE) exit ;;
         *) ;;
     esac
-    echo "%{l}$wm | $title%{c}$date%{r}$battery" || exit # exit if pipe to lemonbar is closed
-done | lemonbar -g x$height -B $barbg -F $barfg -f "Cascadia Mono-10" | sh
+    echo -e "%{l}$wm%{c}$title%{r}$date " || exit # exit if pipe to lemonbar is closed
+done | lemonbar -g x$height -B $barbg -F $barfg -f "Cascadia Mono-9.6" | sh
