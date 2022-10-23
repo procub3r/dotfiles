eb983a783ea06a3e3795e682b7c17ff871f8c6e1
procub3r <procub3r@protonmail.com>
2022-10-23 20:01:55 +0530

    Update neovim config

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -68,7 +68,7 @@ cmp.setup({
     },
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- language server init
 require('lspconfig').clangd.setup {
