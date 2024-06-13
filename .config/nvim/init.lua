37f904417f7f60dd4a8b06980b15375e03a78776
procub3r <procub3r@proton.me>
2024-06-13 17:05:48 +0530

    Update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,29 +1,4 @@
-require 'paq' {
-    'savq/paq-nvim';
-    'sainnhe/sonokai';
-    'rebelot/kanagawa.nvim';
-    'windwp/nvim-autopairs';
-    'neovim/nvim-lspconfig';
-    'cloudhead/neovim-fuzzy';
-    'nvim-treesitter/nvim-treesitter';
-
-    'hrsh7th/nvim-cmp';
-    'hrsh7th/vim-vsnip';
-    'hrsh7th/cmp-vsnip';
-    'hrsh7th/cmp-nvim-lsp';
-}
-
-require 'lsp' -- lua/lsp.lua
-
-require('nvim-autopairs').setup()
-
--- neovim-fuzzy
-vim.g.fuzzy_rootcmds = {'git', 'rev-parse', '--show-toplevel'}
-
-require('nvim-treesitter.configs').setup {
-    highlight = {enable = true}
-}
-
+-- map keybinds first so i still get to use them if something in the config errors out
 vim.g.mapleader = ' '
 vim.keymap.set('n', [[<leader>w]], [[:w<cr>]], {noremap=true})
 vim.keymap.set('n', [[<leader>q]], [[:q<cr>]], {noremap=true})
@@ -32,26 +7,43 @@ vim.keymap.set('n', [[<leader>j]], [[<c-w>j]], {noremap=true})
 vim.keymap.set('n', [[<leader>k]], [[<c-w>k]], {noremap=true})
 vim.keymap.set('n', [[<leader>l]], [[<c-w>l]], {noremap=true})
 vim.keymap.set('n', [[<leader>;]], [[:nohl<cr>]], {noremap=true})
+vim.keymap.set('n', [[<leader>e]], [[:e ]], {noremap=true})
 vim.keymap.set('n', [[<leader>s]], [[:!]], {noremap=true})
-vim.keymap.set('t', [[<esc>]], [[<c-\><c-n>]], {noremap=true}) -- exit terminal with esc key
 vim.keymap.set('n', [[<leader>f]], [[:FuzzyOpen<cr>]], {noremap=true})
 vim.keymap.set('n', [[<leader>g]], [[:FuzzyGrep<cr>]], {noremap=true})
+vim.keymap.set('t', [[<esc>]], [[<c-\><c-n>]], {noremap=true}) -- exit terminal with esc key
+
+require('paq') {
+    'savq/paq-nvim';
+    'rebelot/kanagawa.nvim';
+    'windwp/nvim-autopairs';
+    'cloudhead/neovim-fuzzy';
+    'nvim-treesitter/nvim-treesitter';
+
+    'neovim/nvim-lspconfig';
+    'hrsh7th/cmp-nvim-lsp';
+    'hrsh7th/cmp-buffer';
+    'hrsh7th/cmp-path';
+    'hrsh7th/cmp-cmdline';
+    'hrsh7th/nvim-cmp';
+    'hrsh7th/cmp-vsnip';
+    'hrsh7th/vim-vsnip';
+}
 
 require('kanagawa').setup({
     transparent = true,
+    colors = {theme={all={ui={bg_gutter="none"}}}}
 })
-
 vim.cmd [[colorscheme kanagawa]]
 
--- vim.opt.termguicolors = true
--- vim.g.sonokai_style = 'andromeda'
--- vim.g.sonokai_better_performance = true
--- vim.g.sonokai_transparent_background = false
--- vim.g.sonokai_transparent_background = 1
--- vim.g.sonokai_diagnostic_text_highlight = 1
--- vim.g.sonokai_disable_terminal_colors = true
--- vim.g.sonokai_show_eob = false
--- vim.cmd [[colorscheme sonokai]]
+require('nvim-autopairs').setup()
+require('nvim-treesitter.configs').setup {
+    highlight = {enable = true}
+}
+
+-- highlight trailing whitespace
+vim.cmd [[highlight ExtraWhitespace ctermbg=grey guibg=grey]]
+vim.cmd [[match ExtraWhitespace /\s\+$/]]
 
 vim.opt.title = true
 vim.opt.number = true
@@ -60,14 +52,15 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.guicursor = 'i:block'
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = 'number'
+-- vim.opt.colorcolumn = '81'
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.undofile = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
--- highlight trailing whitespace
-vim.cmd [[highlight ExtraWhitespace ctermbg=grey guibg=grey]]
-vim.cmd [[match ExtraWhitespace /\s\+$/]]
+-- enable lsp at the end so i still get to use nvim with my preferences if lsp errors out on me
+require('lsp')
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,3 +1,7 @@
+local lspconfig = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local cmp = require('cmp')
+
 local on_attach = function(client, bufnr)
     local bufopts = {noremap=true, silent=true, buffer=bufnr}
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
@@ -10,58 +14,38 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>p', vim.lsp.buf.format, bufopts)
+    vim.keymap.set('n', 'gl', vim.diagnostic.open_float, bufopts)
 end
 
-local cmp_default_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-require ('lspconfig').texlab.setup {
-    on_attach = on_attach,
-    capabilities = cmp_default_capabilities
-}
-
-require ('lspconfig').ghcide.setup {
-    on_attach = on_attach,
-    capabilities = cmp_default_capabilities
-}
-
-require ('lspconfig').zls.setup {
-    on_attach = on_attach,
-    capabilities = cmp_default_capabilities
-}
-
-require('lspconfig').clangd.setup {
-    on_attach = on_attach,
-    capabilities = cmp_default_capabilities
-}
-
-require('lspconfig').pyright.setup {
-    on_attach = on_attach,
-    capabilities = cmp_default_capabilities
-}
+local servers = { 'clangd', 'zls', 'rust_analyzer', 'html' }
+for _, server in ipairs(servers) do
+    lspconfig[server].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+end
 
 local has_words_before = function()
+    unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
 local feedkey = function(key, mode)
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
-local cmp = require 'cmp'
 cmp.setup({
     snippet = {
         expand = function(args)
             vim.fn['vsnip#anonymous'](args.body)
         end,
     },
-    sources = cmp.config.sources({
-        {name = 'nvim_lsp'},
-        {name = 'vsnip'}
-    }, {
-        {name = 'buffer'},
-    }),
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
+        ['<c-space>'] = cmp.mapping.complete(),
+        ['<c-e>'] = cmp.mapping.abort(),
+        ['<cr>'] = cmp.mapping.confirm({select = true}),
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
@@ -72,16 +56,35 @@ cmp.setup({
             else
                 fallback()
             end
-        end, {'i', 's'}),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif vim.fn['nsnip#jumpable'](-1) == 1 then
+            elseif vim.fn['vsnip#jumpable'](-1) == 1 then
                 feedkey('<Plug>(vsnip-jump-prev)', '')
             end
-        end, {'i', 's'}),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({select=true}),
-    },
+        end, { 'i', 's' }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'vsnip' },
+        { name = 'path' },
+    }),
 })
+
+-- cmp.setup.cmdline({ '/', '?' }, {
+--     mapping = cmp.mapping.preset.cmdline(),
+--     sources = {
+--         { name = 'buffer' },
+--     }
+-- })
+-- 
+-- cmp.setup.cmdline(':', {
+--     mapping = cmp.mapping.preset.cmdline(),
+--     sources = cmp.config.sources({
+--         { name = 'path' },
+--     }, {
+--         { name = 'cmdline' },
+--     })
+-- })
